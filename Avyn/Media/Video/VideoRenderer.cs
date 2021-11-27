2869a74ff5761e584c8c0e5d298100a28384d8cd
Hazdryx <hazdryx@gmail.com>
2021-11-27 08:26:29 -0600

    Created `VideoRenderer` to have a basic `IVideoStream` implementation.

---
+++ Avyn/Media/Video/VideoRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Hazdryx.Drawing;
+
+namespace Avyn.Media.Video
+{
+    public abstract class VideoRenderer : IVideoStream
+    {
+        /// <summary>
+        ///     Gets the video information for the video being read.
+        /// </summary>
+        public VideoStreamInfo VideoFormat { get; private set; }
+        /// <summary>
+        ///     Gets the current frame index.
+        /// </summary>
+        public int FrameIndex { get; private set; }
+        public bool CanRead => true;
+        public bool CanWrite => false;
+
+        public TimeSpan Position
+        {
+            get => new TimeSpan((long) Math.Round(FrameIndex / VideoFormat.FrameRate * TimeSpan.TicksPerSecond));
+        }
+        public TimeSpan Duration => VideoFormat.Duration;
+
+        public virtual bool ReadFrame(FastBitmap bmp)
+        {
+            if (bmp.Width != VideoFormat.Width || bmp.Height != VideoFormat.Height) throw new ArgumentException("The FastBitmap has invlid dimensions.");
+
+            if (RenderFrame(bmp, Position))
+            {
+                FrameIndex++;
+                return true;
+            }
+            else return false;
+        }
+        public void WriteFrame(FastBitmap bmp) => throw new InvalidOperationException();
+
+        /// <summary>
+        ///     Renders a frame at a specific time.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="time"></param>
+        public abstract bool RenderFrame(FastBitmap target, TimeSpan time);
+        public abstract void Dispose();
+    }
+}
